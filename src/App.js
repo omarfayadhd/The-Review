cb9af60a1785c139c3589549fae66f93664e267c
Omar Fayadh D <canva4994@gmail.com>
2022-08-27 20:55:00 +0530

    API Integration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
 import './App.css';
-
+import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<div className="App">
+  <NavBar/>
+  <Banner/>
+  <RowPost/>
+</div> 
+ );
 }
 
 export default App;
---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Banner.css'
+function Banner() {
+  return (
+    <div className='banner'>
+<div className='content'>
+    <h1 className='title'>Movie Name</h1>
+    <div className='banner_buttons'>
+        <button className='button'>Play</button>
+        <button className='button'>My list</button>
+    </div>
+<h1 className='description'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, reprehenderit!</h1>
+</div>
+<div className='fade_bottom '></div>
+    </div>
+  )
+}
+
+export default Banner
